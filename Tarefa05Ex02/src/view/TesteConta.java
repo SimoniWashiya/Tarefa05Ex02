98758a471f70afb1621a04efb5286f7aec69066a
Simoni Washiya <simoni.washiya@fatec.sp.gov.br>
2024-04-17 04:53:17 -0300

    Update TesteConta.java

--- Tarefa05Ex02/src/view/TesteConta.java
+++ Tarefa05Ex02/src/view/TesteConta.java
@@ -2,19 +2,17 @@ package view;
 
 public class TesteConta {
     public static void main(String[] args) {
-        // Criando uma nova instância da classe Conta
+      
         Conta minhaConta = new Conta();
 
-        // Definindo os valores dos atributos da conta
         minhaConta.setAgencia(1234);
         minhaConta.setCodigo(5678);
         minhaConta.setSaldo(1000);
         minhaConta.setLimite(500);
 
-        // Realizando operações na conta
         System.out.println("Saldo inicial: " + minhaConta.getSaldo());
         minhaConta.deposita(500);
-        System.out.println("Saldo após depósito: " + minhaConta.getSaldo());
+        System.out.println("Saldo apÃ³s depÃ³sito: " + minhaConta.getSaldo());
 
         try {
             minhaConta.saca(2000);
